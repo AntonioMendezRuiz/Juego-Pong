87c98b1633fe0df8ac1e9d8262132ba67015c459
Antonio <mendezruiz20@gmail.com>
2019-11-22 12:28:46 +0100

    Damos mas velocidad

--- js/canvas.js
+++ js/canvas.js
@@ -3,8 +3,8 @@ var ctx = canvas.getContext("2d");
 var ballRadius = 7;
 var x = canvas.width/2;
 var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
+var dx = 5;
+var dy = -5;
 var paddleHeight = 2;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
@@ -59,7 +59,7 @@ function collisionDetection() {
                     b.status = 0;
                     score++;
                     if(score == brickRowCount*brickColumnCount) {
-                        alert("hAS GANADO");
+                        alert("HAS GANADO");
                         document.location.reload();
                     }
                 }
